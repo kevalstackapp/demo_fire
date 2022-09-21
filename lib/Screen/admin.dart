9c77c1ce19ddee3a44055827879df189cfc52f99
SA10 <keval.stackapp@gmail.com>
2022-09-21 13:11:34 +0530

    commit

--- lib/Screen/admin.dart
+++ lib/Screen/admin.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:demo_fire/model/showDataFire.dart';
 import 'package:demo_fire/model/user_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -16,87 +15,31 @@ class admin extends StatefulWidget {
 }
 
 class _adminState extends State<admin> {
-  User? user;
+
   bool staus = false;
   UserModel? userModel;
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    loaddata();
-  }
 
-  loaddata() {
-    FirebaseAuth load = FirebaseAuth.instance;
-    user = load.currentUser;
-    if (user == null) {
-      user!.reload();
-    }
-    setState(() {
-      staus = true;
-    });
-  }
+  String auth  = FirebaseAuth.instance.currentUser!.uid;
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body:  StreamBuilder(
-        stream: FirebaseFirestore.instance.collection("user").snapshots(),
+        stream: FirebaseFirestore.instance.collection("user").doc(auth).snapshots(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: Text('Somthing is Laoding'));
           } else if (snapshot.hasData) {
             final user = snapshot.data!;
-
-            return ListView.builder(
-              itemCount: user.docs.length,
-              itemBuilder: (context, index) {
-                DocumentSnapshot documentSnapshot = user.docs[index];
-
-                UserModel userModel = UserModel.fromJson(
-                    documentSnapshot.data() as Map<String, dynamic>);
-                return ListTile(
-                  trailing: IconButton(
-                      onPressed: () {
-                        showDialog(
-                          context: context,
-                          builder: (context) {
-                            return AlertDialog(
-                                title: Text("Delet Your Acount"),
-                                actions: [
-                                  TextButton(
-                                      onPressed: () {
-                                        final docuser = FirebaseFirestore
-                                            .instance
-                                            .collection('user')
-                                            .doc(documentSnapshot.id);
-                                        docuser.delete();
-                                        Navigator.pop(context);
-                                      },
-                                      child: Text("Yes")),
-                                  TextButton(
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      },
-                                      child: Text("No")),
-                                ]);
-                          },
-                        );
-                      },
-                      icon: Icon(Icons.delete)),
-                  leading: userModel.userImg != null
-                      ? Image.network("${userModel.userImg}")
-                      : Icon(
-                    Icons.manage_accounts_outlined,
-                    size: 50,
-                  ),
-                  title: (userModel.email.toString().isNotEmpty)
-                      ? Text("")
-                      : const Text("No Email"),
-                );
-              },
+            UserModel userModel = UserModel.fromJson(
+                user.data() as Map<String, dynamic>);
+            return ListTile(
+              title: Text("${userModel.email}"),
             );
+
+
           } else {
             return Center(
               child: CircularProgressIndicator(),
